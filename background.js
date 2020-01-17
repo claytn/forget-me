8e8e9e7a9eb56eca0955a8788aea33547eaace8e
marsha88 <marsha88@purdue.edu>
2020-01-16 19:18:18 -0500

    read from chrome sync storage

--- background.js
+++ background.js
@@ -1,9 +1,24 @@
-//background.js
+/** THIS IS TEMORARY - WILL SOON ALLOW USERS TO ADD DOMAINS MANUALLY */
+chrome.storage.sync.set({'domains': ['medium.com']}, function() {
+  console.log('Value is set to ' + 'medium');
+});
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_incognito" ) {
-      chrome.windows.create({url: request.url, incognito: true });
+  (request, sender, sendResponse) => {
+    if(request.message === "open_incognito" ) {
+      chrome.windows.create({ url: request.url, incognito: true });
+    }
+    else if(request.message === "read_storage") {
+      chrome.storage.sync.get(['domains'], (result) => {
+        // Send a message to the active tab
+        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+          const [activeTab] = tabs
+          chrome.tabs.sendMessage(activeTab.id, 
+            { message: "fetched_domain_storage",  storage: result });
+        });
+      });
     }
   }
 );
+
+
--- content.js
+++ content.js
@@ -1,13 +1,30 @@
-// content.js
 
-const incognitoLinks = $("a[href^='https://medium.com']")
-incognitoLinks.toArray().forEach(element => {
-  element.onclick = evt => {
-    evt.preventDefault();
-    chrome.runtime.sendMessage({
-      message: "open_incognito", 
-      url: element.href
-    }) 
+const createIncognitoLinks = domain => {
+  const incognitoLinks = $(`a[href*=\"${domain}\"]`)
+  incognitoLinks.toArray().forEach(element => {
+    element.onclick = evt => {
+      evt.preventDefault();
+      chrome.runtime.sendMessage({
+        message: "open_incognito", 
+        url: element.href
+      }) 
+    }
+  });
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if(request.message === "fetched_domain_storage") {
+      const domains = request.storage && request.storage.domains
+      domains && domains.forEach((domain) => {
+        createIncognitoLinks(domain)
+      })
+    }
   }
-});
+);
+
+chrome.runtime.sendMessage({ message: "read_storage" })
+
+
+
+
 
